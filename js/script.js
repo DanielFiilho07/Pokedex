a457f076736a822e6e540ca8e25d9a6ea7052a91
Daniel Gedeão <daniel_filho_16@hotmail.com>
2022-11-20 19:42:55 -0300

    adicionando script

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const nomePokemon = document.querySelector('.pokemon__nome')
+const idPokemon = document.querySelector('.pokemon__id')
+const imagemPokemon = document.querySelector('.pokemon__image')
+const form = document.querySelector('.form')
+const inputPesquisa = document.querySelector('.input__search')
+const botaoAnterior = document.querySelector('.btn-prev')
+const botaoProximo = document.querySelector('.btn-next')
+
+let primeiroPokemon = 1;
+
+const encontrarPokemon = async  (pokemon) => {
+    const pokemonEncontrado = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+
+    if (pokemonEncontrado.status === 200) {
+      const data = await pokemonEncontrado.json()
+      return data
+    }
+}
+
+const renderizarNaView = async (pokemon) => {
+  nomePokemon.innerHTML = "Carregando..."
+  idPokemon.innerHTML = ''
+
+  const data = await encontrarPokemon(pokemon)
+  
+  if (data) {
+    imagemPokemon.style.display ='block'
+    nomePokemon.innerHTML = data.name
+    idPokemon.innerHTML = data.id
+    imagemPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+    primeiroPokemon = data.id
+  } else {
+    nomePokemon.innerHTML = "Não encontrado"
+    idPokemon.innerHTML = ''
+    imagemPokemon.style.display = 'none'
+  }
+
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  renderizarNaView(inputPesquisa.value.toLowerCase())
+  inputPesquisa.value = ''
+})
+
+botaoAnterior.addEventListener('click', () => {
+  if (primeiroPokemon > 1) {
+    primeiroPokemon -= 1
+    renderizarNaView(primeiroPokemon)
+  }
+});
+
+botaoProximo.addEventListener('click', () => {
+  primeiroPokemon += 1
+  renderizarNaView(primeiroPokemon)
+});
+
+renderizarNaView(primeiroPokemon)
